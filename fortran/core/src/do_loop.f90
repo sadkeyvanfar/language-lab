f449049baba6078454c701dc188a556de8eb28df
Saeid.keyvanfar <sad.keyvanfar@gmail.com>
2025-05-21 01:45:17 +0200

    add label to do_loop.f90

--- fortran/core/src/do_loop.f90
+++ fortran/core/src/do_loop.f90
@@ -4,8 +4,8 @@ program do_loop
   integer :: i, num_time_steps = 100
 
   ! Loop over time steps
-  do i = 1, num_time_steps
+  label: do i = 1, num_time_steps
     print *, "Time step: ", i
-  end do
+  end do label
 
 end program do_loop
