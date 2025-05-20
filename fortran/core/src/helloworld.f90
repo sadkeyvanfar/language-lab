1038704cfb30a0f5342a9ca4b972874a7ec9a9cd
Saeid.keyvanfar <sad.keyvanfar@gmail.com>
2025-05-20 21:37:11 +0200

    add helloworld fortran sample

---
+++ fortran/core/src/helloworld.f90
@@ -0,0 +1,6 @@
+! This is a simple Fortran program that prints "Hello, World!" to the console.
+program helloworld
+  ! not to assume variable types based on their names.
+  implicit none
+  print *, "Hello, World!"
+end program helloworld
