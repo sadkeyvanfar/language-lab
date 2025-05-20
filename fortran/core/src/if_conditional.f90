bd56b33da58df68055b3031c1aa77a69c233dee8
Saeid.keyvanfar <sad.keyvanfar@gmail.com>
2025-05-21 00:50:02 +0200

    add if_conditional.f90

---
+++ fortran/core/src/if_conditional.f90
@@ -0,0 +1,36 @@
+! This program demonstrates the use of conditional statements in Fortran.
+program conditional
+  implicit none
+
+  ! Declare variables
+  integer :: a, b, c
+  logical :: condition
+
+  ! Initialize variables
+  a = 5
+  b = 10
+  c = 15
+
+  ! Check if a is less than b and c is greater than a
+  ! Operators in Fortran:
+  ! .lt.  : less than
+  ! .gt.  : greater than
+  ! .le.  : less than or equal to
+  ! .ge.  : greater than or equal to
+  ! .eq.  : equal to
+  ! .ne.  : not equal to
+  ! .and. : logical AND
+  ! .or.  : logical OR
+  ! .not. : logical NOT
+  ! .neqv.: not equal (logical)
+  ! .eqv. : equal (logical)
+  condition = (a < b) .and. (c > a)
+
+  ! Print the result of the condition
+  if (condition) then
+    print *, "Condition is true: a < b and c > a"
+  else
+    print *, "Condition is false: either a >= b or c <= a"
+  end if
+
+end program conditional
