37981cbdc8cc4f1ae9ed5672ced54a1580cb08a9
Saeid.keyvanfar <sad.keyvanfar@gmail.com>
2025-06-06 22:51:13 +0200

    update module in javascript files

--- javascript/core/src/modules/counter.js
+++ javascript/core/src/modules/counter.js
@@ -1,3 +1,6 @@
+
+// each module is singleton by default
+// if need multiple instances, use a class or factory function
 // counter.js module
 let count = 0;
 export let description = 'Counter module for incrementing a count value';
