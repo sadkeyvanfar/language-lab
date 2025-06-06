8054670fcdd321563177bee3f471962331ffefc3
Saeid.keyvanfar <sad.keyvanfar@gmail.com>
2025-06-06 02:00:55 +0200

    add declaration.js to javascript

---
+++ javascript/core/src/declaration.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var name = "Saeid"; // Using 'var' for a variable that can be redeclared and updated
+const city = "Tehran"; // Using 'const' for a constant value
+
+if (true) {
+    var name = "Saeid";
+    let age = 30; // Using 'let' for a block-scoped variable
+}
+
+console.log(name); // Saeid
+// console.log(age);  // Uncommenting this line will throw an error because 'age' is not defined outside the block
+// city = "Mashhad"; // This will throw an error because 'city' is a constant
+
+const cities = [
+    "Tehran",
+    "Mashhad",
+    "Isfahan"
+];
+cities[2] = "Tom Cruise"; // OK :(
+// cities = []; // Error! Assignment to a constant variable
--- javascript/core/src/object-type.js
+++ javascript/core/src/object-type.js
@@ -1,18 +1,18 @@
 "use strict";
 
 // Objects in JavaScript can be of various types, including arrays and functions
-var person = {
-    first: "Kyle",
-    last: "Simpson",
-    age: 39,
-    specialties: [ "JS", "Table Tennis" ],
-    functional: function() {
-        return "This is a method in an object";
-    }
-};
 
-console.log("Person first name:", person.first); // Accessing properties of an object
-console.log("Person specialties first element:", person.specialties[0]); // Accessing elements in an array property of an object
+var book = {
+    title: "JS",
+    isbn: "978-0596805524",
+    tags: [ "JavaScript", "Programming", "Web Development" ],
+    read: function() {
+        return "Reading the book: " + this.title;
+    }
+}
+
+console.log("Book title:", book.title); // Accessing properties of an object
+console.log("Book first tag:", book.tags[0]); // Accessing elements in an array property of an object
 
 var arr = [1, 2, 3];
 console.log("Array length:", arr.length); // Arrays are also objects, and have a length property
@@ -33,7 +33,7 @@ var expFunc = function() {
 // Arrow function expression
 var arrowFunc = () => "This is an arrow expression function"; // Arrow functions are a concise syntax for writing functions
 
-console.log("Object:", person, "type", typeof person);
+console.log("Object:", book, "type", typeof book);
 console.log("Array:", arr, "type", typeof arr);
 console.log("decFunc:", decFunc(), "type", typeof decFunc);
 console.log("expFunc:", expFunc(), "type", typeof expFunc);
