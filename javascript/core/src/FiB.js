fdbcfe5e64d9b8f57f054dcdefcf4ed0d4f880d9
Saeid.keyvanfar <sad.keyvanfar@gmail.com>
2025-06-07 23:29:44 +0200

    add FiB.js to javascript files

---
+++ javascript/core/src/FiB.js
@@ -0,0 +1,11 @@
+// Fib: Function Declarations in Blocks
+// This code demonstrates the behavior of function declarations within blocks.
+// Note: This behavior can vary across different JavaScript environments.
+
+{
+  function sayHello() {
+    console.log("Hello");
+  }
+
+  sayHello(); // Works in some environments, but not all!
+}
