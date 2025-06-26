1487e28d1ac74e678807fcd57c817fc2e9e71a9a
Saeid.keyvanfar <sad.keyvanfar@gmail.com>
2025-06-27 00:51:39 +0200

    add AbstractClass.java to Java

---
+++ java/core/src/main/java/ir/java/basic/AbstractClass.java
@@ -0,0 +1,21 @@
+package ir.java.basic;
+
+abstract class Vehicle {
+    abstract void start();
+}
+
+/**
+ * This class demonstrates the use of abstract classes in Java.
+ * It includes an abstract class Vehicle with an abstract method start,
+ * and a subclass Car that implements the start method.
+ */
+class Car extends Vehicle {
+    void start() {
+        System.out.println("Car starting");
+    }
+
+    public static void main(String[] args) {
+        Vehicle v = new Car();
+        v.start();
+    }
+}
