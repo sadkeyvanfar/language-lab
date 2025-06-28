27b21e1e806f19b82d130ac9da832ca1458f56b6
Saeid.keyvanfar <sad.keyvanfar@gmail.com>
2025-06-28 23:20:29 +0200

    add literal.pl to Perl

---
+++ perl/core/bin/type/literal.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $x = 12345; # integer
+my $y = 3.14;  # floating-point number
+my $x = 6.02e24; # scientific notation
+my $x = 6_394_767_256; # underline for legibility
+my $x = 0737; # octal
+my $x = 0xffff; # hexadecimal
+my $x = 0b1100_0010; # binary
+my $name = "Perl"; # string
+my $name = 'Perl'; # string
+my $name = `Perl`; # string
+my $is_active = 1; # boolean (1 for true, 0 for false)
+my $undefined; # undefined value
