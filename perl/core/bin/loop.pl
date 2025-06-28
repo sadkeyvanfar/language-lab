0484e19b734facba34e01d613698b82f4d5a4c9b
Saeid.keyvanfar <sad.keyvanfar@gmail.com>
2025-06-28 23:04:31 +0200

    update loop.pl with next, last and redo examples

--- perl/core/bin/loop.pl
+++ perl/core/bin/loop.pl
@@ -43,3 +43,27 @@ do {
     print "Count is $count\n";  # Print the current count
     $count++;  # Increment the count
 } while ($count < 2);  # Continue until count is less than 2
+
+# next and last statements
+print "\n=== Next and Last Statements ===\n";
+for (my $i = 0; $i < 5; $i++) {
+    if ($i == 2) {
+        print "Skipping iteration $i\n";  # Skip the iteration when i is 2
+        next;  # Skip to the next iteration, Like continue in C/Java.
+    }
+    if ($i == 4) {
+        print "Breaking at iteration $i\n";  # Break the loop when i is 4
+        last;  # Exit the loop, Like break in C/Java.
+    }
+    print "Iteration $i\n";  # Print the current iteration number
+}
+
+# redo statement
+print "\n=== Redo Statement ===\n";
+for (my $i = 0; $i < 5; $i++) {
+    if ($i == 2) {
+        print "Redoing iteration $i\n";  # Redo the iteration when i is 2
+        redo;  # Redo the current iteration
+    }
+    print "Iteration $i\n";  # Print the current iteration number
+}
