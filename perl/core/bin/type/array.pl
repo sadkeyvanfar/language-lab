944c3ec092a29c733972f5e4571e1c258421ab7b
Saeid.keyvanfar <sad.keyvanfar@gmail.com>
2025-06-28 22:08:19 +0200

    add array.pl to Perl

---
+++ perl/core/bin/type/array.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# Declare an array of colors
+my @colors = ("red", "green", "blue");
+
+# Print the array
+print "Colors: @colors\n";
+
+# Access a single element (scalar context)
+print "First color: $colors[0]\n";
+
+# Get the number of elements
+my $count = scalar @colors;
+print "Number of colors: $count\n";
+
+# Add a new element
+push @colors, "yellow";
+print "After adding yellow: @colors\n";
+
+# Remove the last element
+my $last = pop @colors;
+print "Removed last color: $last\n";
+print "Colors now: @colors\n";
