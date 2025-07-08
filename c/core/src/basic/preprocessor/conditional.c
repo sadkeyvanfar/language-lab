82af57a7b438c92dfabe244e543e7ce248857c55
Saeid.keyvanfar <sad.keyvanfar@gmail.com>
2025-07-08 22:01:15 +0200

    clean conditional.c macro in c language

--- c/core/src/basic/preprocessor/conditional.c
+++ c/core/src/basic/preprocessor/conditional.c
@@ -5,24 +5,24 @@
 // to include or exclude code based on certain conditions.
 
 int main() {
-    // Define a macro for conditional compilation
-    #define DEBUG 1
+// Define a macro for conditional compilation
+#define DEBUG 1
 
-    // Use the macro to conditionally compile code
-    #if DEBUG
-        printf("Debug mode is ON\n");
-    #elif defined(DEBUG) && DEBUG == 0
-        printf("Debug mode is OFF\n");
-    #else
-        printf("Debug mode is not defined\n");
-    #endif
+// Use the macro to conditionally compile code
+#if DEBUG
+    printf("Debug mode is ON\n");
+#elif defined(DEBUG) && DEBUG == 0
+    printf("Debug mode is OFF\n");
+#else
+    printf("Debug mode is not defined\n");
+#endif
 
-    // Another example of conditional compilation
-    #ifdef FEATURE_X
-        printf("Feature X is enabled\n");
-    #else
-        printf("Feature X is not enabled\n");
-    #endif
+// Another example of conditional compilation
+#ifdef FEATURE_X
+    printf("Feature X is enabled\n");
+#else
+    printf("Feature X is not enabled\n");
+#endif
 
     return 0;
 }
